00dfcd4579b641981ce8a99c994329fa756bbd4e
Shayne Tuel <shaynetuel@gmail.com>
2020-11-17 15:23:55 -0500

    Update solver-gui.py
    Updated main gui method with final tweek

--- solver-gui.py
+++ solver-gui.py
@@ -202,24 +202,24 @@ class Cube:
         self.temp = val
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return (i, j)  # row, col
 
     return None
 
 
-def valid(bo, num, pos):
+def valid(board, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
 
     # Check box
@@ -228,7 +228,7 @@ def valid(bo, num, pos):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if board[i][j] == num and (i,j) != pos:
                 return False
 
     return True
@@ -344,4 +344,5 @@ def main():
 
 main()
 pygame.quit()
+
 
